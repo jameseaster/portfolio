5e41df6757da68508bd7a214046f18498800b854
James Easter <james.m.easter@gmail.com>
2023-05-21 14:54:25 -0600

    Fix netlify redirects.

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,9 +1,5 @@
-const CONSTANTS = {
-  lambda: "https://ighe6y2962.execute-api.us-east-2.amazonaws.com/lambdaSES",
-};
-
 const ACTIONS: { [key: string]: string } = {
   UPDATE_ANIMATION_TRACKER: "UPDATE_ANIMATION_TRACKER",
 };
 
-export { ACTIONS, CONSTANTS };
+export { ACTIONS };
